eb3bb3b810896542455600337aa4d62d9b13e1c5
Ricco <riccorashuamanuni23@gmail.com>
2025-09-18 16:30:03 -0500

    Subiendo archivo de Python

---
+++ codigo.py
@@ -0,0 +1,10 @@
+nombre = "Lucas"
+apellido = "dalto"
+
+<<<<<<< HEAD
+print("hola " + nombre + " " + apellido + " ¿Cómo estas?")
+=======
+saludo = "hola " + nombre + " " + apellido
+
+print(saludo)
+>>>>>>> conflict-branch
